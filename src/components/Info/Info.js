c8dfe5b9e3e6b90435e718b9ec831bfb69cccd9c
Keroro2139 <k.chulaket@gmail.com>
2018-04-09 16:05:11 +0700

    add position

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react'
 const Theme = {
     lightTheme: {
         head: '#202020',
+        position: '#202020',
         icon: '#3F68B5',
         text: '#666666',
         hr: '#E5E5E5',
     },
     darkTheme: {
         head: '#E2E2E2',
+        position: '#E2E2E2',
         icon: '#E41E26',
         text: '#E7E7E7',
         hr: '#5F5F5F',
@@ -38,10 +40,16 @@ class Info extends Component {
             name: {
                 fontSize: 30,
                 fontWeight: 'bold',
-                marginBottom: 30,
+                marginBottom: 0,
                 textTransform: 'uppercase',
                 letterSpacing: -1,
                 color: `${Theme[Mode].head}`,
+            },
+            position: {
+                fontSize: 28,
+                marginBottom: 30,
+                letterSpacing: 1,
+                color: `${Theme[Mode].position}`,
             },
             icon: {
                 marginRight: 20,
@@ -62,6 +70,7 @@ class Info extends Component {
         return (
             <div>
                 <h1 style={styles.name}>{Information[Language].name}</h1>
+                <h3 style={styles.position}>Cloud Developer Engineer</h3>
                 <div style={styles.flex}>
                     <i className="fas fa-home" style={styles.icon}></i>
                     <p style={styles.detailStyle}>{Information[Language].address}</p>
@@ -84,7 +93,7 @@ class Info extends Component {
                         <p style={styles.detailStyle}>https://github.com/Keroro2139</p>
                     </a>
                 </div>
-                <hr style={{backgroundColor: `${Theme[Mode].hr}`}}/>
+                <hr style={{ backgroundColor: `${Theme[Mode].hr}` }} />
             </div>
         )
     }
